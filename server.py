9bc71d6ce6587a71e6ea5251c5309155210e6c00
Jonas <jonas.remgard@gmail.com>
2016-10-09 15:59:25 +0800

    Beggining of the server side

--- server.py
+++ server.py
@@ -1,32 +1,63 @@
+#!/usr/bin/env python
+import time
+import random
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+from uuid import uuid4
 
-import tornado.ioloop
-import tornado.web
-import tornado.websocket
-import tornado.template
+async_mode = None
 
-class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-    loader = tornado.template.Loader(".")
-    self.write(loader.load("index.html").generate())
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
 
-class WSHandler(tornado.websocket.WebSocketHandler):
-  def open(self):
-    print ('connection opened...')
-    self.write_message("The server says: 'Hello'. Connection was accepted.")
+names = ["Jonas", "Jerry", "Morre", "Anton", "Trulle", "Herman"]
+online_players = {}
 
-  def on_message(self, message):
-    self.write_message("The server says: " + message + " back at you")
-    print ('received:', message)
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
 
-  def on_close(self):
-    print ('connection closed...')
 
-application = tornado.web.Application([
-  (r'/ws', WSHandler),
-  (r'/', MainHandler),
-  (r"/(.*)", tornado.web.StaticFileHandler, {"path": "./resources"}),
-])
+@socketio.on('connect', namespace='/game')
+def test_connect():
+    session['id'] = str(uuid4())
 
-if __name__ == "__main__":
-  application.listen(8080)
-  tornado.ioloop.IOLoop.instance().start()
+    name = random.choice(names) + "-" + str(random.randint(1, 9999))
+    opos = {"x": 400, "y": 300, "name": name, "type": "player",
+            "direction": "right", "id": session['id']}
+    emit('identifed', opos, namespace='/game')
+    socketio.emit('newplayer', opos, namespace='/game')
+
+    for player_name in online_players.keys():
+        data = online_players[player_name]
+        emit('newplayer', data, namespace='/game')
+
+    print("Client connected", session["id"], "given name:", name)
+    online_players[session['id']] = opos
+    # print "added:", name
+
+
+@socketio.on('disconnect', namespace='/game')
+def on_disconnect():
+    print("Disconnected", session["id"])
+    online_players.pop(session["id"], None)
+    socketio.emit("player_disconnected", session["id"], namespace='/game')
+
+
+@socketio.on('ping', namespace='/game')
+def on_ping():
+    socketio.emit('pong', None, namespace='/game')
+
+
+@socketio.on('move', namespace='/game')
+def move(message):
+    print("Moved!", message)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ static/js/socket.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+            console.log("JavaScript loaded");
+
+            namespace = '/game';
+
+            var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+
+            socket.on('connect', function() {
+                console.log("Connected");
+                $("#status").text("Connected");
+            });
+
+            var ping_pong_times = [];
+            var start_time;
+            window.setInterval(function() {
+                start_time = (new Date).getTime();
+                socket.emit('ping');
+                socket.on('pong', function() {
+                    var latency;
+                    latency = Date.now() - start_time;
+                    $("#ping-pong").text("Your ping: " + latency + " ms");
+                });
+            }, 750);
+
+            socket.on('global_message', function(message) {
+              console.log("Global message: " + message);
+            });
+
+            socket.on('player_disconnected', function(id) {
+                console.log("disconnected_player_id: " + id);
+            });
+
+            socket.on('identifed', function(data) {
+                var player;
+                console.log("new player connected " + data.name);
+            });
+
+            socket.on('newplayer', function(data) {
+                var player;
+                console.log("new player connected " + data.id);
+            });
+
+
+            moveUp = function() {
+                socket.emit("move", {
+                    "action": "up",
+                    "id": "id",
+                    "type": "player"
+                });
+            };
+
+            moveDown = function() {
+                socket.emit("move", {
+                    "action": "down",
+                    "id": "id",
+                    "type": "player"
+                });
+            };
+
+            moveLeft = function() {
+                socket.emit("move", {
+                    "action": "left",
+                    "id": "id",
+                    "type": "player"
+                });
+            };
+
+            moveRight = function() {
+                socket.emit("move", {
+                    "action": "right",
+                    "id": "id",
+                    "type": "player"
+                });
+            };
+        });
